5f2f57fcd34f8c8f2f2b2859cfb2d08b4c913d2b
maryrehnaca-cmd <maryrehnaca@gmail.com>
2026-07-09 15:46:53 +0530

    First_trial

---
+++ grade.js
@@ -0,0 +1,22 @@
+let grade = "A";
+
+switch(score){
+    case "A":
+        console.log("Very Good");
+        break;
+    case "B":
+        console.log("Good");
+        break;
+    case "C":
+        console.log("Average");
+        break;
+    case "D":
+        console.log("Bad");
+        break;
+    case "E":
+        console.log("Not Eligible");
+        break;
+    default:
+        console.log("Invalid entry!!!")
+
+}
